8791ea0d6a206a678ca506a4b5e9a24947ae7e1b
student <kooduser@pupil.kood.tech>
2025-03-11 19:56:55 +0200

    1

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -4,3 +4,4 @@ const supabaseUrl = 'https://ivwgjrhuqiovrvtwmytd.supabase.co';
 const supabaseAnonKey = 'YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2d2dqcmh1cWlvdnJ2dHdteXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2OTE2MTgsImV4cCI6MjA1NzI2NzYxOH0.6ydnxk_EuIIZddLp2Ilzn5QxJ8p69F6JQlvtuW1G2Ik';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);*/
+.
